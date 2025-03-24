3ec8fabceae68c3c258161b3a9799675856b38af
samuel <timeuss.craft@gmail.com>
2025-03-24 23:54:17 +0100

    player creation

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ const canvas = document.querySelector('canvas')
 
 const c = canvas.getContext('2d')
 
-console.log('c', c)
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.width = 30
+    this.height = 30
+  }
+
+  // interaction with canvas
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const player = new Player()
+player.draw()
